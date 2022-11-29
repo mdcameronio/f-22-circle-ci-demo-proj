e1c98ce3f9706cdffa0eab6f04324d35b554fddd
sdev301 <cameron.mathieu@student.greenriver.edu>
2022-11-29 13:45:02 -0800

    new project

---
+++ src/test/java/com/example/f22circleciproject2/F22CircleCiProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.f22circleciproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F22CircleCiProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
