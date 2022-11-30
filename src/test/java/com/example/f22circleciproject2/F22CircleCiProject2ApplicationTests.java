e44d397a2e86f3625f0848c1379436d695ec8fa1
sdev301 <cameron.mathieu@student.greenriver.edu>
2022-11-30 09:56:26 -0800

    added test

--- src/test/java/com/example/f22circleciproject2/F22CircleCiProject2ApplicationTests.java
+++ src/test/java/com/example/f22circleciproject2/F22CircleCiProject2ApplicationTests.java
@@ -1,13 +1,43 @@
 package com.example.f22circleciproject2;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class F22CircleCiProject2ApplicationTests {
 
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate template;
+
+
     @Test
-    void contextLoads() {
+    void contextLoads()
+    {
+
+    }
+    @Test
+    public void homeTest()
+    {
+        String url = "http://localhost:" + port + "/api/v1/home";
+        HttpEntity request = new HttpEntity(new HttpHeaders());
+        ResponseEntity<String> response = template.exchange(url, HttpMethod.GET,
+                request, String.class);
+
+        //test whether it worked or not?!?!
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        assertEquals(response.getBody(), "Hello, world!");
     }
 
 }
